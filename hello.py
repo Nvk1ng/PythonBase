f25db35e728afa6235893f86e598adba1a3806e1
Matheus Calmon <sanderhus15@outlook.com>
2025-09-24 10:31:12 -0300

    exercicios usando dicionario

--- hello.py
+++ hello.py
@@ -16,14 +16,28 @@ Execução:
     ou
     ./hello.py
 """
-__version__ = "0.0.1"
+__version__ = "0.1.2"
 __author__ = "Matheus Sanderhus"
 __license__ = "unlicense"
 
 import os 
 
 current_language = os.getenv("LANG", "en_US")[:5]
-msg = "Hello, World!"
+
+# sets (Hash Table) - O(1) - Constante
+# dicts (Hash Table) O(1)
+
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Olá Mundo!",
+    "es_SP": "Hola, Mundo!",
+    "fr_FR": "Bonjour, Monde!",
+}
+
+print(msg[current_language])
+
+
+""" Ordem de complexidade O(n)
 
 if current_language == "pt_BR":
     msg = "Olá Mundo!"
@@ -33,5 +47,4 @@ elif current_language == "es_SP":
     msg = "Hola, Mundo!"
 elif current_language == "fr_FR":
     msg = "Bonjour, Monde!"
-
-print(msg)
+ """
